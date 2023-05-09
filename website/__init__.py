aedccf058b9839f6bafcb69876e97efbc52b27a6
Joao Ribeiro <joaobjjribeiro@hotmail.com>
2023-05-09 16:20:08 -0400

    updated

--- website/__init__.py
+++ website/__init__.py
@@ -5,9 +5,10 @@ from os import path
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'asdfghjkl'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -18,14 +19,14 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-
+    
     with app.app_context():
         db.create_all()
 
     return app
 
+
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app = app)
-        print('Database Created!')
-
+        db.create_all(app=app)
+        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        
 
     return render_template('login.html')
 
@@ -17,22 +20,23 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
         elif len(password1) < 7:
             flash('Password must be greater than 6 characters.', category='error')
         elif password2 != password1:
             flash('Password does not match.', category='error')
         else:
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
